a0d3c308a6bcf9512ef6cb85b62b51be3e7fdc9a
Seker <sekermac@gmail.com>
2024-07-22 16:03:02 +0200

    Version 1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FloatingButton from './components/FloatingButton';
+
+const App = () => {
+    return (
+        <div className="App">
+            <FloatingButton />
+        </div>
+    );
+};
+
+export default App;
---
+++ src/components/FloatingButton.js
@@ -0,0 +1,13 @@
+// src/FloatingButton.js
+import React from 'react';
+import './FloatingButton.css';
+
+const FloatingButton = () => {
+    return (
+        <div className="screen">
+            <button className="floating-button">Öffne die Box</button>
+        </div>
+    );
+};
+
+export default FloatingButton;
